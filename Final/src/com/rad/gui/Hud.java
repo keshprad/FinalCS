da131da76e7b8c3589c5b6bec6ae37b3c991992a
daniel <danieltomelee64@gmail.com>
2020-04-25 20:54:11 -0700

    Added HUD class and new maps

---
+++ Final/src/com/rad/gui/Hud.java
@@ -0,0 +1,25 @@
+package com.rad.gui;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import com.rad.world.GameWorld;
+
+public class Hud {
+	
+	private GameWorld gameWorld;
+	
+	public Hud(GameWorld gameWorld) {
+		this.gameWorld = gameWorld;
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render(Graphics g) {
+//		g.setColor(Color.DARK_GRAY);
+//		g.fillRect(20, 20, 600, 60);
+	}
+	
+}
