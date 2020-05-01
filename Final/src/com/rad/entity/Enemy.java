07a954422239aef471a3a3161e03135ab9e11961
aksh11335 <akshanth.srivatsa@gmail.com>
2020-05-01 04:22:13 -0700

    Comments on enemy and removed Menu temporarily

--- Final/src/com/rad/entity/Enemy.java
+++ Final/src/com/rad/entity/Enemy.java
@@ -108,37 +108,47 @@ public class Enemy extends Entity {
     	//Implementing speed(the field)
     	
         PriorityQueue<Location> pq = new PriorityQueue<Location>((o1, o2) -> Double.compare(o1.priority, o2.priority));
-        // creates a priority queue of possible points that the person can go to
+        // creates a priority queue of possible nodes that the enemy can go to
+        //(o1, o2) -> Double.compare(o1.priority, o2.priority) basically tells the PriorityQ to compare through the compare method
+        //the higher up the node is on the priorityQ the more favorable the location is for the enemy
+        //
 
         HashMap<Location, Double> map = new HashMap<>();
         Location startingLoc = new Location(this.x, this.y, 0, null, 0);
         pq.add(startingLoc);// adds the position you are in
         Location playerLocation = new Location(player.x, player.y, 0);
-        map.put(startingLoc, 0.0);
+        map.put(startingLoc, 0.0);//puts the player into the map
         while (!pq.isEmpty()) {
-            Location pt = pq.poll();
+            Location pt = pq.poll();// first iteration of the loop removes first node, rest helps determine
             Rectangle rectangle = new Rectangle(pt.getX(), pt.getY(), TILE_SIZE, TILE_SIZE);
-            if (rectangle.intersects(player.getBounds())) //this line needs to have getbounds collision
+            if (rectangle.intersects(player.getBounds())) //if the enemy caught the player
             {
-                if (pt.movePoint != null) {
+                if (pt.movePoint != null)
+                {
                     move(pt.movePoint);
                 }
                 break;
             }
-            for (Location possiblePostion : pt.possibleMoveLocations(gameWorld, pt, this)) {
+            //checks every possible move position in the tile in the gameworld with this enemy
+            for (Location possiblePostion : pt.possibleMoveLocations(gameWorld, pt, this))
+            {
                 if (possiblePostion != null) {
-                    Location movePoint = pt.movePoint;
+                    Location movePoint = pt.movePoint;// if the location
                     if (movePoint == null) {
                         movePoint = possiblePostion;
                     }
                     double dist = pt.dist + pt.distBetween(possiblePostion);
-                    double priority = possiblePostion.distBetween(playerLocation) + dist;
-                    Location neighbor = new Location(possiblePostion.getX(), possiblePostion.getY(), dist, movePoint, priority);
+                    double priority = possiblePostion.distBetween(playerLocation) + dist; //hueristic to determine the priority of the A* algorithim which is
+                    //basically travel distance  so far + distance to Location
+                    Location neighbor = new Location(possiblePostion.getX(), possiblePostion.getY(), dist, movePoint, priority); //highest priority should be the nieghbor to the enemy
 
-                    if (!map.containsKey(neighbor)) {
+                    if (!map.containsKey(neighbor)) // if the map of all possible tiles does not contain this neighbor nessary for not including repeats
+                    {
                         map.put(possiblePostion, priority);
                         pq.add(neighbor);
-                    } else if (map.get(possiblePostion) > priority) {
+                    } else if (map.get(possiblePostion) > priority) //if the possible postition has a higher priority than whats given,
+                        // where all of them would be put on the first run, put in map of all possible positions
+                    {
                         map.put(possiblePostion, priority);
                         pq.remove(neighbor);
                         pq.add(neighbor);
--- Final/src/com/rad/world/Menu.java
+++ Final/src/com/rad/world/Menu.java
@@ -10,33 +10,33 @@ import static com.rad.world.GameWorld.StateOfGame;
 import static com.rad.world.GameWorld.StateOfGame.PLAYINGGAME;
 
 public class Menu extends MouseAdapter {
-    public void mousePressed(MouseEvent e){
-        int mouseXLocation=e.getX();
-        int mouseYLocation=e.getY();
-        if(mouseIsOverLocation((xCoordinateForBox,yCoordinateForBox, widthOfBox, heightOfBox))
-        {
-            GameWorld.gameState= PLAYINGGAME;
-        }
-    }
-    public int xCoordinateForBox=0;
-    public int yCoordinateForBox=0;
-    public int heightOfBox=0;
-    public int widthOfBox=0;
-    public boolean mouseIsOverLocation(int mouseX, int mouseY, int x, int y, int width, int height)
-    {
-        return ((mouseX>x&&mouseX<x+width)&&(mouseY>y&&mouseY<y+height));
-    }
-    public void mouseReleased(MouseEvent e)
-    {
-
-    }
-    public void tick(){}
-    public void render(Graphics g){
-        Font f1=new Font("comic sans",20,19);
-        g.setColor(Color.GRAY);
-        g.drawRect(xCoordinateForBox,yCoordinateForBox, widthOfBox, heightOfBox);
-
-    }
+//    public void mousePressed(MouseEvent e){
+//        int mouseXLocation=e.getX();
+//        int mouseYLocation=e.getY();
+//        if(mouseIsOverLocation((xCoordinateForBox,yCoordinateForBox, widthOfBox, heightOfBox))
+//        {
+//            //GameWorld.gameState= PLAYINGGAME;
+//        }
+//    }
+//    public int xCoordinateForBox=0;
+//    public int yCoordinateForBox=0;
+//    public int heightOfBox=0;
+//    public int widthOfBox=0;
+//    public boolean mouseIsOverLocation(int mouseX, int mouseY, int x, int y, int width, int height)
+//    {
+//        return ((mouseX>x&&mouseX<x+width)&&(mouseY>y&&mouseY<y+height));
+//    }
+//    public void mouseReleased(MouseEvent e)
+//    {
+//
+//    }
+//    public void tick(){}
+//    public void render(Graphics g){
+//        Font f1=new Font("comic sans",20,19);
+//        g.setColor(Color.GRAY);
+//        g.drawRect(xCoordinateForBox,yCoordinateForBox, widthOfBox, heightOfBox);
+//
+//    }
 
 
 }
